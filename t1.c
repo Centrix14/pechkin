0969bcf6ec78da76d0d5b804dfbd1aa77677afeb
Centrix14 <artiom.zvyagin@yandex.ru>
2021-03-17 19:07:48 +0300

    test

---
+++ t1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "st.h/st.h"
+#include "pl.h"
+
+st_name("t1");
+st_debug_start(1);
+
+int main() {
+	st_start();
+
+	char *names[] = {"It", "is", "a", "Pechkin", "test"};
+	char *values[] = {"It-msg", "is-msg", "a-msg", "Pechkin-msg", "test-msg"};
+
+	for (int i = 0; i < 5; i++)
+		pl_send(names[i], values[i], strlen(values[i]));
+
+	st_end();
+	return 0;
+}
