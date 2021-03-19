e01c512a4c86fc4a2ac55f0d27a2878e41b779d2
Centrix14 <artiom.zvyagin@yandex.ru>
2021-03-19 17:49:20 +0300

    new tests

---
+++ t2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "tl2/list.h"
+#include "st.h/st.h"
+#include "pl.h"
+
+st_name("t2");
+st_debug_start(1);
+
+int main() {
+	st_start();
+
+	char *names[] = {"It", "is", "a", "Pechkin", "test"};
+	char *values[] = {"It-msg", "is-msg", "a-msg", "Pechkin-msg", "test-msg"};
+
+	for (int i = 0; i < 5; i++)
+		pl_send(names[i], values[i], strlen(values[i]) + 1);
+	for (int i = 0; i < 5; i++)
+		printf("| %s : %s\n", names[i], (char*)pl_read(names[i]));
+
+	st_end();
+	return 0;
+}
